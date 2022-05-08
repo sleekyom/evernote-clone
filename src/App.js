64488a4f37992b185aeb795ded11aa75395fdc49
Emmanuel Kolawole <amdas72@gmail.com>
2022-05-09 00:39:25 +0300

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 
 function App() {
